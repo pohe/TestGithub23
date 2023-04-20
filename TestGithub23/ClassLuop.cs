685b065329d2035e6be59073a017a7e83713a091
Poul Henriksen <pohe@zealand.dk>
2023-04-20 16:59:57 +0200

    Made ClassLuop

---
+++ TestGithub23/ClassLuop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub23
+{
+    internal class ClassLuop
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+        public ClassLuop()
+        {
+            Age = 23;
+            Name = "Peter";
+        }
+    }
+}
